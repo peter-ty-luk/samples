152cd0227efa920684e2ec4484ee2303504a7575
Peter Luk <peter.ty.luk@gmail.com>
2020-07-20 00:21:39 +0800

    Update with latest annotation GetMapping

--- springboot-rest/src/main/java/com/peterluk/samples/springbootrest/GreetingController.java
+++ springboot-rest/src/main/java/com/peterluk/samples/springbootrest/GreetingController.java
@@ -1,14 +1,14 @@
 package com.peterluk.samples.springbootrest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GreetingController {
 
-    @RequestMapping("/greeting")
-    @ResponseBody
+    @GetMapping("/greeting")
     public String greeting() {
         return "Hello World";
     }
